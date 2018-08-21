0bf7a21ae1e2c56964b60ac627e7bce6266d5f63
mariancator <42573518+mariancator@users.noreply.github.com>
2018-08-21 19:06:11 +0800

    Register.php

---
+++ .gitignore/.gitignore/Register.php
@@ -0,0 +1,15 @@
+<?php
+    $con = mysqli_connect("http://localhost/phpmyadmin/", "id6866706_user", "mcator3390", "id6866706_mcator");
+    
+    $name = $_POST["name"];
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    $statement = mysqli_prepare($con, "INSERT INTO user (name, username, password) VALUES (?, ?, ?)");
+    mysqli_stmt_bind_param($statement, "siss", $name, $username, $password);
+    mysqli_stmt_execute($statement);
+    
+    $response = array();
+    $response["success"] = true;  
+    
+    echo json_encode($response);
+?>
